cee7ed857334a4b4569aca0a2f2295ef9fa4064f
Michael Lang <michael.langh-da.de>
2022-10-14 12:20:48 +0200

    quick

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,13 +1,6 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-    't3up',
-    'Configuration/TsConfig/Quickedit.typoscript',
-    'EXT:t3up: Quickedit'
-);
-
 $pagesColumns = [
     'showtitle' => [
         'exclude' => true,
