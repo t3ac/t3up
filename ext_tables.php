c4247f7f61fd2d5a51ee40c4718cbd1908f5c352
Michael Lang <113889180+t3ac@users.noreply.github.com>
2024-05-23 17:27:31 +0200

    v12.05

--- ext_tables.php
+++ ext_tables.php
@@ -1,30 +1,21 @@
 <?php
 
-/*
- * This file is part of the package T3UP-package.
- *
- * For the full copyright and license information, please read the
- * LICENSE file that was distributed with this source code.
- */
+defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
 
-defined('TYPO3_MODE') or die();
+# $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['cs_presets'] = 'EXT:t3up/Configuration/Rte/CsPresets.yaml';
+
+$GLOBALS['TCA']['pages']['columns']['title']['config']['max'] = 80;
+$GLOBALS['TCA']['pages']['columns']['title']['config']['size'] = 30;
+$GLOBALS['TCA']['pages']['columns']['nav_title']['config']['max'] = 50;
+$GLOBALS['TCA']['pages']['columns']['nav_title']['config']['size'] = 30;
+
+$GLOBALS['TCA']['tt_content']['columns']['imagecols']['config']['default'] = 1;
+$GLOBALS['TCA']['tt_content']['columns']['header']['config']['type'] = 'text';
+$GLOBALS['TCA']['tt_content']['columns']['header']['config']['cols'] = '60';
+$GLOBALS['TCA']['tt_content']['columns']['header']['config']['rows'] = '2';
+$GLOBALS['TCA']['tt_content']['columns']['header']['config']['eval'] = 'required';
 
 
-$boot = static function (): void {
-    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['cs_presets'] = 'EXT:t3up/Configuration/Rte/CsPresets.yaml';
-    
-    $GLOBALS['TCA']['pages']['columns']['title']['config']['max'] = 80;
-    $GLOBALS['TCA']['pages']['columns']['title']['config']['size'] = 30;
-    $GLOBALS['TCA']['pages']['columns']['nav_title']['config']['max'] = 50;
-    $GLOBALS['TCA']['pages']['columns']['nav_title']['config']['size'] = 30;
-    
-    $GLOBALS['TCA']['tt_content']['columns']['imagecols']['config']['default'] = 1;
-    $GLOBALS['TCA']['tt_content']['columns']['header']['config']['type'] = 'text';
-    $GLOBALS['TCA']['tt_content']['columns']['header']['config']['cols'] = '60';
-    $GLOBALS['TCA']['tt_content']['columns']['header']['config']['rows'] = '2';
-    $GLOBALS['TCA']['tt_content']['columns']['header']['config']['eval'] = 'required';
-    
-};
 
 (function () {
     
@@ -43,6 +34,3 @@ $boot = static function (): void {
         );
 })();
 
-    
-$boot();
-unset($boot);
