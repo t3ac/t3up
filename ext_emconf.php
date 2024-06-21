5e8263c30beccb52d6cd3a07ef1025db7a988bf6
Michael Lang <info@mediadesign-ffm.de>
2024-06-21 19:34:32 +0200

    v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array (
     array (
         'depends' =>
         array (
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '11.4',
         ),
         'conflicts' =>
         array (
