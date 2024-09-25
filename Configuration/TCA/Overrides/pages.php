0e9f713360123727de673d8b1a462c57eba08b29
Michael Lang <113889180+t3ac@users.noreply.github.com>
2024-09-25 22:22:16 +0200

    Update pages.php

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,43 +1,31 @@
 <?php
-defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
+defined('TYPO3_MODE') or die();
 
-(static function (): void {
-    
-    $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
-        'Brofix',
-        'brofix',
-        'apps-pagetree-folder-contains-brofix',
-    ];
-    $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-brofix']
-    = 'apps-pagetree-folder-contains-brofix';
-    
-})();
-
-$pagesColumns = array(
+$pagesColumns = [
     'showtitle' => [
         'exclude' => true,
         'label' => 'LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:showtitle',
         'config' => [
-            'type' => 'check',
-            'default' => 0
-        ]
-    ],
+                'type' => 'check',
+                'default' => 0
+            ]
+        ],
     'deltitle' => [
         'exclude' => true,
         'label' => 'LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:deltitle',
         'config' => [
-            'type' => 'check',
-            'default' => 0
-        ]
-    ],
+                'type' => 'check',
+                'default' => 0
+            ]
+        ],
     'breadcrumb' => [
         'exclude' => true,
         'label' => 'LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:breadcrumb',
         'config' => [
-            'type' => 'check',
-            'default' => 0
-        ]
-    ],
+                'type' => 'check',
+                'default' => 0
+            ]
+        ],
     'hidenav' => [
         'exclude' => true,
         'label' => 'LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:hidenav',
@@ -45,7 +33,7 @@ $pagesColumns = array(
             'type' => 'check',
             'default' => 0
         ]
-    ],
+    ],   
     'nonav' => [
         'exclude' => true,
         'label' => 'LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:nonav',
@@ -53,46 +41,44 @@ $pagesColumns = array(
             'type' => 'check',
             'default' => 0
         ]
-    ],
-    'pagecolor' => [
-        'exclude' => true,
-        'label' => 'LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:pagecolor',
-        'config' => [
-            'type' => 'select',
-            'renderType' => 'selectSingle',
-            'items' => [
-                ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:pagecolor-no', ''],
-                ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:pagecolor-blue', 'blue'],
-                ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:pagecolor-orange', 'orange'],
-                ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:pagecolor-red', 'red'],
-                ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:pagecolor-green', 'green'],
-                ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:pagecolor-violett', 'violett'],
-                ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:pagecolor-yellow', 'yellow'],
-                ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:pagecolor-grey', 'grey'],
-                ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:pagecolor-turquoise', 'turquoise'],
-            ],
-            'default' => ''
-        ]
+    ],   
+    'basecolor' => [
+           'exclude' => true,
+           'label' => 'LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:basecolor',
+           'config' => [
+               'type' => 'select',
+               'renderType' => 'selectSingle',
+               'items' => [
+                   ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:basecolor-no', ''],
+                   ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:basecolor-blue', 'blue'],
+                   ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:basecolor-orange', 'orange'],
+                   ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:basecolor-red', 'red'],
+                   ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:basecolor-green', 'green'],
+                   ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:basecolor-violett', 'violett'],
+                   ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:basecolor-yellow', 'yellow'],
+                   ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:basecolor-grey', 'grey'],          
+                   ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:basecolor-turquoise', 'turquoise'], 
+                   ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:basecolor-darkblue', 'darkblue'],        
+                   ['LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:basecolor-darkgrey', 'darkgrey'],                   
+                   ],
+               'default' => ''
+           ]
     ],
     'previousnext' => [
         'exclude' => true,
         'label' => 'LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:previousnext ',
         'config' => [
-            'type' => 'check',
-            'default' => 0
-        ]
-    ],
-    
-);
+                'type' => 'check',
+                'default' => 0
+            ]
+        ],
+        
+];
 
-
-$GLOBALS['TCA']['pages']['palettes']['t3up'] = array(
+$GLOBALS['TCA']['pages']['palettes']['t3up'] = [
     'canNotCollapse' => true,
-    'showitem' => 'pagecolor,--linebreak--,showtitle,deltitle,--linebreak--,breadcrumb,previousnext,--linebreak--,hidenav,nonav'
-);
-
+    'showitem' => 'basecolor,--linebreak--,showtitle,deltitle,--linebreak--,breadcrumb,previousnext,--linebreak--,hidenav,nonav'
+];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $pagesColumns);
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages', 'xtrafunction,--palette--;LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf:palette;t3up', '', 'after:title');
-
-
