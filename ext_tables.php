d1254bdf2b89daeb711d8db3c5be1daedcc07760
Michael Lang <113889180+t3ac@users.noreply.github.com>
2024-10-24 22:31:17 +0200

    12.0.01

--- ext_tables.php
+++ ext_tables.php
@@ -2,8 +2,6 @@
 
 defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
 
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['cs_presets'] = 'EXT:t3up/Configuration/Rte/CsPresets.yaml';
-
 $GLOBALS['TCA']['pages']['columns']['title']['config']['max'] = 80;
 $GLOBALS['TCA']['pages']['columns']['title']['config']['size'] = 30;
 $GLOBALS['TCA']['pages']['columns']['nav_title']['config']['max'] = 50;
@@ -14,23 +12,3 @@ $GLOBALS['TCA']['tt_content']['columns']['header']['config']['type'] = 'text';
 $GLOBALS['TCA']['tt_content']['columns']['header']['config']['cols'] = '60';
 $GLOBALS['TCA']['tt_content']['columns']['header']['config']['rows'] = '2';
 $GLOBALS['TCA']['tt_content']['columns']['header']['config']['eval'] = 'required';
-
-
-
-(function () {
-    
-    //register backend module
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'T3ac\T3up',
-        'Brofix',
-        '',
-        '',
-        [],
-        [
-            'icon'   => 'EXT:t3up/Resources/Public/Icons/brofix.svg',
-            'access' => 'user,group',
-            'labels' => 'LLL:EXT:t3up/Resources/Private/Language/locallang_backend.xlf',
-        ]
-        );
-})();
-
