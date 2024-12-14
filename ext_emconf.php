5542da460509e4db2b2103019f16966c02b57fe5
Michael Lang <113889180+t3ac@users.noreply.github.com>
2024-12-14 15:14:23 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@
 $EM_CONF[$_EXTKEY] = array (
     'title'            => 'T3UP - Basic Installation',
     'description'      => 'T3UP - Distribution.',
-    'version'          => '12.0.01',
+    'version'          => '12.0.05',
     'state'            => 'stable',
     'category'         => 'templates',
     'author'           => 'Michael Lang',
