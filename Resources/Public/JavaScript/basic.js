087782ab42d0b9423e2ccd6a316af353deeb13a8
Michael Lang <113889180+t3ac@users.noreply.github.com>
2025-04-30 20:04:37 +0200

    30.04.

--- Resources/Public/JavaScript/basic.js
+++ Resources/Public/JavaScript/basic.js
@@ -1,7 +1,5 @@
 jQuery(document).ready(function($) {
-	
-    
-   var $body = $('body'),
+	var $body = $('body'),
         $stH = $('.sticky-header').height();
         $stH = $('header.fixed').height();
 	
@@ -140,7 +138,7 @@ jQuery(document).ready(function($) {
 	footer = $('footer').height();
 	
 	if ( $(window).width() < 576 ) {
-	   	$('.trigger').click(function(){
+	   	$('.triggable > .trigger').click(function(){
 			$(this).next().slideToggle( "slow" );
 			$(this).toggleClass('active');
 			if ($(this).attr('aria-expanded') === 'true' ) {
