e9403be682f158377e5a6ddf9470a1d41e06d7a1
Michael Lang <113889180+t3ac@users.noreply.github.com>
2026-03-31 12:53:49 +0200

    Delete Resources directory

--- Resources/Public/JavaScript/chars4.js
+++
@@ -1,4 +0,0 @@
-var	$plu    = unescape("%uF138"),
-    $min    = unescape("%uF137"),
-    $pluTop = unescape("%uF13a"),
-    $minTop = unescape("%uF139");
--- Resources/Public/JavaScript/wheninview.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-
-	 $('.content > div').whenInView();
-
-});
